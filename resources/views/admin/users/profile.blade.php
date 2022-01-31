60c5ada07c28b5a98878d80d45933106d78280c1
dav24github <Davidthbsossa@gmail.com>
2022-01-30 23:53:23 -0400

    Storage image fix

--- resources/views/admin/users/profile.blade.php
+++ resources/views/admin/users/profile.blade.php
@@ -1,18 +1,96 @@
 <x-master.admin-master>
-    @section('content')
-        <form method="POST" action="{{route('user.profile.test')}}">
-            @csrf
-          <select name="checkboxArray" id="">
-              <option value="asd">Delete</option>
-          </select>
 
-          <input name="Bottun-input" type="submit">
-          <br>
-          @foreach ($users as $user)
-            <input type="checkbox" name="checkBoxArray[]" value="{{$user->name}}">
-            <label for="checkBoxArray[]">{{$user->name}}</label><br>
-          @endforeach
-        </form>
+    @section('content')
+
+        <h1>User Profile for : {{$user->name}}</h1>
+        <div class="row">
+            <div class="col-sm-6">
+                <form method="post" action="{{route('user.profile.update', $user)}}" enctype="multipart/form-data">
+                    @csrf
+                    @method('PUT')
+
+                    <div class="mb-4">
+                        <img class="img-profile rounded-circle" width="300" src="{{$user->avatar}}">
+                    </div>
+
+                    <div class="form-group">
+                        <input type="file" name="avatar">
+                    </div>
+
+
+                    <div class="form-group">
+                        <label for="username">Username</label>
+                        <input type="text"
+                                name="username"
+                                class="form-control @error('username') is-invalid @enderror"
+                                id="username"
+                                value="{{$user->username}}"
+                        >
+                        @error('username')
+                        <div class="invalid-feedback">{{$message}}</div>
+                        @enderror
+                    </div>
+
+
+
+                    <div class="form-group">
+                        <label for="name">Name</label>
+                        <input type="text"
+                                name="name"
+                                class="form-control @error('name') is-invalid @enderror"
+                                id="name"
+                                value="{{$user->name}}"
+                        >
+                        @error('name')
+                        <div class="alert alert-danger">{{$message}}</div>
+                        @enderror
+                    </div>
+
+
+                    <div class="form-group">
+                        <label for="email">Email</label>
+                        <input type="text"
+                                name="email"
+                                class="form-control @error('email') is-invalid @enderror"
+                                id="email"
+                                value="{{$user->email}}"
+                        >
+
+                        @error('email')
+                        <div class="alert alert-danger">{{$message}}</div>
+                        @enderror
+                    </div>
+
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input type="password"
+                                name="password"
+                                class="form-control @error('password') is-invalid @enderror"
+                                id="password"
+                        >
+                        @error('password')
+                        <div class="alert alert-danger">{{$message}}</div>
+                        @enderror
+                    </div>
+
+
+                    <div class="form-group">
+                        <label for="password-confirmation">Confirm Password</label>
+                        <input type="password"
+                                name="password_confirmation"
+                                class="form-control @error('password_confirmation') is-invalid @enderror"
+                                id="password-confirmation"
+                        >
+                        @error('password_confirmation')
+                        <div class="alert alert-danger">{{$message}}</div>
+                        @enderror
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+
+                </form>
+            </div>
+       </div>
+
     @endsection
 
 </x-master.admin-master>
--- resources/views/components/master/admin-master.blade.php
+++ resources/views/components/master/admin-master.blade.php
@@ -90,12 +90,12 @@
         </div>
 
         <!-- Nav Item - Pages Collapse Menu -->
-        <li class="nav-item active">
-          <a class="nav-link" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true" aria-controls="collapsePages">
+        <li class="nav-item">
+          <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true" aria-controls="collapsePages">
             <i class="fas fa-fw fa-folder"></i>
             <span>Pages</span>
           </a>
-          <div id="collapsePages" class="collapse show" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+          <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
             <div class="bg-white py-2 collapse-inner rounded">
               <h6 class="collapse-header">Login Screens:</h6>
               <a class="collapse-item" href="login.html">Login</a>
